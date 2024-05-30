1a9a1e378f0498558bc1871423890997bfc771f3
João Carlos Borges <joaocarlosifsc@hotmail.com>
2024-05-30 11:02:48 -0300

    estilizando a barra lateral

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,19 @@
-const BarraLateral = () => <aside>barra lateral</aside>
+import FiltroCard from '../../components/FiltroCard'
+
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar Contato" />
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard ativo />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral
